960c6fa138820ef9d9440faaeafb1618aaadac6f
martijn van de ree <mrtjnvanderee@gmail.com>
2021-12-06 10:58:13 +0100

    add birds /birds

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use('/', Router);
+app.use('/birds', Router);
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
