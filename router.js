59c7e3d1f26b23157ac908596d107c33337148cb
martijn van de ree <mrtjnvanderee@gmail.com>
2021-12-06 10:43:29 +0100

    router1

--- router.js
+++ router.js
@@ -3,12 +3,9 @@ var router = express.Router();
 
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-router.get('/post1', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/post1.html'));
-});
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+router.get('/post1', (req, res) => res.sendFile(path.join(__dirname, '/public/post1.html')))
 
 
 
